9c3515106641e03a1e1d77e02a98bf140ebe2a75
Reshma PS <reshmaps156@gmail.com>
2024-05-22 11:00:40 +0530

    html,css and js part added to react page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 
 import './App.css';
 import styles from './myStyles.module.css'
+import './Example'
+import './Random'
+import Example from './Example';
+import Random from './Random';
 
 
 function App() {
   //js code 
   const h1Style = {color:'blue',fontSize : '70px'}
+  const subject = 'react'
+  const username = 'maxwell'
   return (
     //html code
     <>
@@ -15,6 +21,12 @@ function App() {
       </div>
       <h1 className={styles.fondFifty}>component</h1>
       <p className={[styles.myStyle,styles.styleBlue].join(' ')}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque laboriosam dolor, voluptatem placeat corporis illum sed temporibus perferendis, dolorem iste asperiores sunt officiis quas distinctio fugit nihil at quasi maxime nulla numquam! Placeat voluptatem dolorum sapiente sed optio odit iste. Aspernatur dolor laborum iste saepe aliquid recusandae aut, perferendis reprehenderit? Nam et animi repudiandae, laudantium reiciendis repellendus doloremque officia sint quam officiis aspernatur maxime quidem dolorem vel nulla labore ea vero voluptate fuga voluptatum eius aperiam! Repudiandae, inventore excepturi mollitia commodi aliquid amet aut quos quasi alias distinctio nisi cupiditate atque ad, eius impedit? Reiciendis possimus iusto fuga distinctio laboriosam.</p>
+      <h3>my subject is {subject}</h3>
+      <h3>user name is {username}</h3>
+      <ul>
+        <li><Example/></li>
+        <li><Random/></li>
+      </ul>
   
       
     </>
---
+++ src/Random.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Random extends Component {
+  render() {
+    return (
+      <div>Random</div>
+    )
+  }
+}
+
+//return should be given with in render method
+//Components should be inherit inorder to recognize by react as class component otherwise its mearly a class
+// 'export default Random' to export the module
